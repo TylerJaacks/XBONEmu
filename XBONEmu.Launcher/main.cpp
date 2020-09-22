2c56be1cdfb19c3e158d4b9cfb265984752d6d7d
Tyler Jaacks <TylerJaacks@Gmail.com>
2020-09-21 20:34:20 -0500

    Added some code.

--- XBONEmu.Launcher/main.cpp
+++ XBONEmu.Launcher/main.cpp
@@ -4,7 +4,22 @@
 #include <vector>
 #include <Windows.h>
 
+
 int main(int argc, char* argv[])
+{	
+	if (argc != 3)
+	{
+		printf("Usage XBONEmu.Launcher.exe -executable <PATH_TO_EXECUTABLE>");
+	}
+
+	std::string executablePath = argv[2];
+
+	
+
+	return 0;
+}
+
+void loadPEImage(std::string executablePath)
 {
 	
 }
